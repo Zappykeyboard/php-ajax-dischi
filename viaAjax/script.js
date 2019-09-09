f04faa063e1c5574a12381f82aa8c035bba11cba
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-09 17:05:08 +0200

    aggiunta logica per stampare gli album

--- viaAjax/script.js
+++ viaAjax/script.js
@@ -10,10 +10,35 @@ function getData(){
     url: 'api.php',
     method: 'GET',
     success: function(data){
-      console.log(data);
+      printAlbums(data);
     },
     error: function(err){
       console.log(err);
     }
   })
+}
+
+function printAlbums(albums){
+
+  //template handlebars
+  var templateHTML = $("#item-template").html();
+  var template = Handlebars.compile(templateHTML);
+
+  var theContainer = $(".container");
+
+  for(var i = 0; i < albums.length; i++){
+    var album = albums[i];
+
+    var context = {
+      name: album.name,
+      author: album.author,
+      image: album.image
+    };
+
+    var html = template(context);
+
+    theContainer.append(html);
+
+  }
+
 }
