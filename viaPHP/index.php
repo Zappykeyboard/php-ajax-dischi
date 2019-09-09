408d64f435017932e720912fa11677ff3e41f7d2
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-09 16:48:23 +0200

    rimossa call inutilizzata

--- viaPHP/index.php
+++ viaPHP/index.php
@@ -8,12 +8,6 @@
     <!-- JS: JQUERY -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <!-- JS: HANDLEBARS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
-    <!-- TEMPLATE: MESSAGE MENU -->
-    <script id="item-template" type="text/x-handlebars-template">
-    </script>
-
     <!-- CSS: MY STYLE -->
     <link rel="stylesheet" href="style.css">
     <!-- JS: MY SCRIPT -->
