ae879ec9a6dad609106d18fe4702b1802c052915
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-10 08:37:31 +0200

    aggiunte date

--- viaAjax/api.php
+++ viaAjax/api.php
@@ -5,37 +5,44 @@
     [
       'name' => 'Highway to hell',
       'author' => 'The handsome devils',
-      'image' => 'img/album_1.jpg'
+      'image' => 'img/album_1.jpg',
+      'release_date' => '2000/10/30'
     ],
     [
       'name' => 'Smash song hits',
       'author' => 'Rodgers & Hart',
-      'image' => 'img/album_2.jpg'
+      'image' => 'img/album_2.jpg',
+      'release_date' => '1990/02/05'
     ],
     [
       'name' => 'Tattoos',
       'author' => 'Jason Derulo',
-      'image' => 'img/album_3.jpg'
+      'image' => 'img/album_3.jpg',
+      'release_date' => '2010/05/20'
     ],
     [
       'name' => 'Blurryface',
       'author' => 'Twentyone Pilots',
-      'image' => 'img/album_4.png'
+      'image' => 'img/album_4.png',
+      'release_date' => '2017/11/15'
     ],
     [
       'name' => 'Fear of the dark',
       'author' => 'Iron Maiden',
-      'image' => 'img/album_5.jpg'
+      'image' => 'img/album_5.jpg',
+      'release_date' => '1992/05/11'
     ],
     [
       'name' => 'Greatest hits II',
       'author' => 'Queen',
-      'image' => 'img/album_6.jpg'
+      'image' => 'img/album_6.jpg',
+      'release_date' => '2000/03/10'
     ],
     [
       'name' => 'Plastic beach',
       'author' => 'Gorillaz',
-      'image' => 'img/album_7.jpg'
+      'image' => 'img/album_7.jpg',
+      'release_date' => '2010/03/03'
     ]
     ];
 
